b1bb1f5a2eddde11f3f4a58a14f3bf04d124c6c2
FindMoney85 <bilastois@gmail.com>
2026-07-01 21:40:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ except ImportError:
     pykap = None
 
 # --- TELEGRAM AYARLARI ---
-TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 TELEGRAM_CHAT_ID = os.environ.get('TELEGRAM_CHAT_ID')
 
 # Statik yedek liste: KAP'a hiçbir şekilde erişilemezse (online=True ve
